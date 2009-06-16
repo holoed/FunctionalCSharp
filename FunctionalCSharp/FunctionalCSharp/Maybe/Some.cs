972437acd751e512827025f6ec1720bb21ab0824
holoed_cp <holoed@users.noreply.github.com>
2009-06-16 21:21:17 +0000

    Added support for matching Maybe Some or None:
    Func<Maybe<int>, bool>
                isFortyTwo = n => n.Match()
                                   .Some(42, () => true)
                                   .Some(    x  => false)
                                   .None(    () => false)
                                   .Return<bool>();

---
+++ FunctionalCSharp/FunctionalCSharp/Maybe/Some.cs
@@ -0,0 +1,32 @@
+#region License
+
+/* ****************************************************************************
+ * Copyright (c) Edmondo Pentangelo. 
+ *
+ * This source code is subject to terms and conditions of the Microsoft Public License. 
+ * A copy of the license can be found in the License.html file at the root of this distribution. 
+ * By using this source code in any fashion, you are agreeing to be bound by the terms of the 
+ * Microsoft Public License.
+ *
+ * You must not remove this notice, or any other, from this software.
+ * ***************************************************************************/
+
+#endregion
+
+namespace FunctionalCSharp
+{
+    public class Some<T> : Maybe<T>
+    {
+        private readonly T _value;
+
+        public Some(T value)
+        {
+            _value = value;
+        }
+
+        public T Value
+        {
+            get { return _value; }
+        }
+    }
+}
---
+++ FunctionalCSharp/FunctionalCSharp/Pattern Matching/MaybeActivePatterns.cs
@@ -0,0 +1,42 @@
+﻿#region License
+
+/* ****************************************************************************
+ * Copyright (c) Edmondo Pentangelo. 
+ *
+ * This source code is subject to terms and conditions of the Microsoft Public License. 
+ * A copy of the license can be found in the License.html file at the root of this distribution. 
+ * By using this source code in any fashion, you are agreeing to be bound by the terms of the 
+ * Microsoft Public License.
+ *
+ * You must not remove this notice, or any other, from this software.
+ * ***************************************************************************/
+
+#endregion
+
+using System;
+
+namespace FunctionalCSharp
+{
+    public static class MaybeActivePatterns
+    {
+        public static PatternMatching<Maybe<T>> Some<T>(this PatternMatching<Maybe<T>> x, Func<T, object> f)
+        {
+            return x.With<Maybe<T>>(b => b is Some<T>, b => f(Value(b)));
+        }
+
+        public static PatternMatching<Maybe<T>> Some<T>(this PatternMatching<Maybe<T>> x, T expected, Func<object> f)
+        {
+            return x.With<Maybe<T>>(b => b is Some<T> && Value(b).Equals(expected), _ => f());
+        }
+
+        public static PatternMatching<Maybe<T>> None<T>(this PatternMatching<Maybe<T>> x, Func<object> f)
+        {
+            return x.With<Maybe<T>>(b => b is None<T>, b => f());
+        }
+
+        private static T Value<T>(Maybe<T> b)
+        {
+            return ((Some<T>)b).Value;
+        }
+    }
+}
