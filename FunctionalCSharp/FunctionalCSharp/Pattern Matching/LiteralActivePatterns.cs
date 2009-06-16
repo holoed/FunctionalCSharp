927234ab3634b7a2ec7108f0b7c14898dae0db5d
holoed_cp <SND\holoed_cp>
2009-06-16 21:47:09 +0000

    Added Literal Active Patterns:
    Func<string, bool>
     IsTrueOrFalse = n => n.Match()
                                             .Value("Yes", () => true)
                                             .Value("False", () => false)
                                             .Return<bool>();            
    Func<int, bool>
     isFiveNotTwelve = n => n.Match()
                                                   .Value(5, () => true)
                                                   .Value(12, () => false)
                                                   .Return<bool>();

---
+++ FunctionalCSharp/FunctionalCSharp/Pattern Matching/LiteralActivePatterns.cs
@@ -0,0 +1,27 @@
+﻿#region License
+
+/* ****************************************************************************
+ * Copyright (c) Edmondo Pentangelo. 
+ *
+ * This source code is subject to terms and conditions of the Microsoft Public License. 
+ * A copy of the license can be found in the License.html file at the root of this distribution. 
+ * By using this source code in any fashion, you are agreeing to be bound by the terms of the 
+ * Microsoft Public License.
+ *
+ * You must not remove this notice, or any other, from this software.
+ * ***************************************************************************/
+
+#endregion
+
+using System;
+
+namespace FunctionalCSharp
+{
+    public static class LiteralActivePatterns
+    {
+        public static PatternMatching<T> Value<T>(this PatternMatching<T> x, T expected, Func<object> f)
+        {
+            return x.With<T>(b => Equals(expected, b), b => f());
+        }
+    }
+}
