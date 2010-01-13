e269f76d2adf3dd5d7d1db9faa0268d47be37af2
holoed_cp <holoed@users.noreply.github.com>
2010-01-13 21:55:51 +0000

    Added Tuples Active Patterns

--- FunctionalCSharp/FunctionalCSharpTests/Tuples/TupleTests.cs
+++ FunctionalCSharp/FunctionalCSharpTests/Tuples/TupleTests.cs
@@ -44,5 +44,20 @@ namespace FunctionalCSharpTests.Tuples
             Assert.AreNotEqual(Tuple.New(12, "435"), Tuple.New("435", 12));
             Assert.AreEqual(Tuple.New(new DateTime(2010, 1, 1), "5"), Tuple.New(new DateTime(2010, 1, 1), "5"));
         }
+
+        [Test]
+        public void ToString3Test()
+        {
+            var tuple = Tuple.New(12, "435", new DateTime());
+            Assert.AreEqual("12, \"435\", 01/01/0001 00:00:00", tuple.ToString());
+        }
+
+        [Test]
+        public void Equals3Test()
+        {
+            Assert.AreEqual(Tuple.New(12, "435", new DateTime()), Tuple.New(12, "435", new DateTime()));
+            Assert.AreNotEqual(Tuple.New(12, "435", new DateTime()), Tuple.New("435", 12, new DateTime()));
+            Assert.AreEqual(Tuple.New(new DateTime(2010, 1, 1), "5", 45.5), Tuple.New(new DateTime(2010, 1, 1), "5", 45.5));
+        }
     }
 }
