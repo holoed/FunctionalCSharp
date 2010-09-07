f3f2d37fa1aa25e7339a90c1f1e40d4487227060
holoed_cp <holoed@users.noreply.github.com>
2010-09-07 21:22:26 +0000

    Added initial support for C# Discriminated Unions

---
+++ FunctionalCSharp/FunctionalCSharp/DiscriminatedUnions/PatternMatchingDataTypeExtensions.cs
@@ -0,0 +1,57 @@
+﻿#region License
+
+/* ****************************************************************************
+ * Copyright (c) Edmondo Pentangelo. 
+ *
+ * This source code is subject to terms and conditions of the Microsoft Public License. 
+ * A copy of the license can be found in the License.html file at the root of this distribution. 
+ * By using this source code in any fashion, you are agreeing to be bound by the terms of the 
+ * Microsoft Public License.
+ *
+ * You must not remove this notice, or any other, from this software.
+ * ***************************************************************************/
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace FunctionalCSharp.DiscriminatedUnions
+{
+    public static class PatternMatchingDataTypeExtensions
+    {      
+        public static PatternMatching<a> With<a, b>(this PatternMatching<a> host, Expression<Func<a, Func<b, object>>> method, Func<b, object> handler)
+        {
+            return host.With<a>(
+                t => t.GetType().Name == GetMethodName(method),
+                t1 => handler.DynamicInvoke(t1.GetType().GetFields().Select(f => f.GetValue(t1)).ToArray()));
+        }
+       
+        public static PatternMatching<a> With<a, b, c>(this PatternMatching<a> host, Expression<Func<a, Func<b, c, object>>> method, Func<b, c, object> handler)
+        {
+            return host.With<a>(
+                t => t.GetType().Name == GetMethodName(method),
+                t1 => handler.DynamicInvoke(t1.GetType().GetFields().Select(f => f.GetValue(t1)).ToArray()));
+        }
+
+        public static PatternMatching<a> With<a, b, c, d>(this PatternMatching<a> host, Expression<Func<a, Func<b, c, d, object>>> method, Func<b, c, d, object> handler)
+        {
+            return host.With<a>(
+                t => t.GetType().Name == GetMethodName(method),
+                t1 => handler.DynamicInvoke(t1.GetType().GetFields().Select(f => f.GetValue(t1)).ToArray()));
+        }
+
+        private static string GetMethodName(Expression method)
+        {
+            return method.Match()
+                .Lambda((args, body) => GetMethodName(body))
+                .Unary(GetMethodName)
+                .MethodCall((mi, ps) => GetMethodName(ps.ElementAt(2)))
+                .Const(c => ((MethodInfo)c).Name)
+                .Return<string>();
+        }
+    }
+}
---
+++ FunctionalCSharp/FunctionalCSharpTests/DiscriminatedUnions/DiscriminatedUnionsTests.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+#region License
+
+/* ****************************************************************************
+ * Copyright (c) Edmondo Pentangelo. 
+ *
+ * This source code is subject to terms and conditions of the Microsoft Public License. 
+ * A copy of the license can be found in the License.html file at the root of this distribution. 
+ * By using this source code in any fashion, you are agreeing to be bound by the terms of the 
+ * Microsoft Public License.
+ *
+ * You must not remove this notice, or any other, from this software.
+ * ***************************************************************************/
+
+#endregion
+
+using System;
+using FunctionalCSharp;
+using FunctionalCSharp.DiscriminatedUnions;
+using NUnit.Framework;
+
+namespace FunctionalCSharpTests.DiscriminatedUnions
+{
+    [TestFixture]
+    public class DiscriminatedUnionsTests
+    {
+        public interface Exp
+        {
+            Exp Var(string x);
+            Exp Lam(string x, Exp e);
+            Exp Let(string x, Exp e1, Exp e2);
+            Exp App(Exp e1, Exp e2);
+        }
+
+        [Test]
+        public void CreateDataTypeAndVisitUsingPatternMatching()
+        {
+            var exp = DataType.New<Exp>();
+
+            exp = exp.Lam("x", exp.Var("x"));
+
+            Func<Exp, string> toString = null;
+            toString = expr => expr.Match()
+                .With(o => o.Var, (string name) => name)
+                .With(o => o.Lam, (string name, Exp e) => string.Format("fun {0} -> {1}", name, toString(e)))
+                .Return<string>();
+
+            Assert.AreEqual("fun x -> x", toString(exp));
+        }
+
+        [Test]
+        public void CreateDataTypeAndVisitUsingPatternMatching2()
+        {
+            var exp = DataType.New<Exp>();
+
+            exp = exp.Let("f", exp.Lam("x", exp.Var("x")), exp.Var("f"));                        
+
+            Func<Exp, string> toString = null;
+            toString = expr => expr.Match()
+                .With(o => o.Var, (string x) => x)
+                .With(o => o.Let, (string x, Exp e1, Exp e2) => string.Format("let {0} = {1} in {2}", x, toString(e1), toString(e2)))
+                .With(o => o.Lam, (string x, Exp e) => string.Format("fun {0} -> {1}", x, toString(e)))
+                .Return<string>();
+
+            Assert.AreEqual("let f = fun x -> x in f", toString(exp));
+        }
+
+        [Test]
+        public void CreateDataTypeAndVisitUsingPatternMatching3()
+        {
+            var exp = DataType.New<Exp>();
+
+            exp = exp.Let("compose",
+                                     exp.Lam("f",
+                                             exp.Lam("g",
+                                                     exp.Lam("x",
+                                                             exp.App(exp.Var("g"), exp.App(exp.Var("f"), exp.Var("x")))))),
+                                     exp.Var("compose"));
+
+            Func<Exp, string> toString = null;
+            toString = expr => expr.Match()
+                .With(o => o.Var, (string x) => x)
+                .With(o => o.Let, (string x, Exp e1, Exp e2) => string.Format("let {0} = {1} in {2}", x, toString(e1), toString(e2)))
+                .With(o => o.Lam, (string x, Exp e) => string.Format("fun {0} -> {1}", x, toString(e)))
+                .With(o => o.App, (Exp e1, Exp e2) => string.Format("({0} {1})", toString(e1), toString(e2)))
+                .Return<string>();
+
+            Assert.AreEqual("let compose = fun f -> fun g -> fun x -> (g (f x)) in compose", toString(exp));
+        }
+    }
+}
